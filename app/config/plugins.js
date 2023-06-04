929d6229dfc138b4ebe854253bf22ae8a1aa955a
vianviet2502 <vianviet2502@gmail.com>
2023-06-04 23:58:47 +0700

    Build docker for strapi v4 unlimit size file

---
+++ app/config/plugins.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      breakpoints: {
+        xxlarge: 10000,
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64,
+      },
+      providerOptions: {
+        localServer: {
+          maxage: 30000000,
+        },
+      },
+      sizeLimit: 3000 * 1024 * 1024, // 256mb in bytes
+    },
+  },
+});
